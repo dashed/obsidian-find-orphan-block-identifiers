16690de6c285731da29809ac70d56ceb05baac7d
Alberto Leal <mail4alberto@gmail.com>
2023-09-03 12:04:53 -0400

    update

--- main.tsx
+++ main.tsx
@@ -1,114 +1,119 @@
 import {
-	App,
-	Modal,
-	Notice,
-	Plugin,
-	PluginSettingTab,
-	Setting,
+    App,
+    Modal,
+    Notice,
+    Plugin,
+    PluginSettingTab,
+    Setting,
 } from "obsidian";
 import { createRoot, Root } from "react-dom/client";
 import * as React from "react";
+import { useContext } from "react";
 
 interface MyPluginSettings {
-	mySetting: string;
+    mySetting: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: "default",
+    mySetting: "default",
 };
 
 export default class AppPlugin extends Plugin {
-	settings: MyPluginSettings;
+    settings: MyPluginSettings;
 
-	async onload() {
-		await this.loadSettings();
+    async onload() {
+        await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		// TODO: remove?
-		const _ribbonIconEl = this.addRibbonIcon(
-			"dice",
-			"Sample Plugin",
-			(evt: MouseEvent) => {
-				// Called when the user clicks the icon.
-				new Notice("This is a notice!");
-			}
-		);
+        // This creates an icon in the left ribbon.
+        // TODO: remove?
+        const _ribbonIconEl = this.addRibbonIcon(
+            "dice",
+            "Sample Plugin",
+            (evt: MouseEvent) => {
+                // Called when the user clicks the icon.
+                new Notice("This is a notice!");
+            }
+        );
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText("Status Bar Text");
+        // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+        const statusBarItemEl = this.addStatusBarItem();
+        statusBarItemEl.setText("Status Bar Text");
 
-		this.addCommand({
-			id: "open-find-find-orphan-block-identifiers-modal",
-			name: "Open",
-			callback: () => {
-				new AppModal(this.app).open();
-			},
-		});
+        this.addCommand({
+            id: "open-find-find-orphan-block-identifiers-modal",
+            name: "Open",
+            callback: () => {
+                new AppModal(this.app).open();
+            },
+        });
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new AppSettingTab(this.app, this));
-	}
+        // This adds a settings tab so the user can configure various aspects of the plugin
+        this.addSettingTab(new AppSettingTab(this.app, this));
+    }
 
-	onunload() {}
+    onunload() {}
 
-	async loadSettings() {
-		this.settings = Object.assign(
-			{},
-			DEFAULT_SETTINGS,
-			await this.loadData()
-		);
-	}
+    async loadSettings() {
+        this.settings = Object.assign(
+            {},
+            DEFAULT_SETTINGS,
+            await this.loadData()
+        );
+    }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 }
 
+const AppContext = React.createContext<App | undefined>(undefined);
+const useApp = (): App | undefined => {
+    return useContext(AppContext);
+};
+
 class AppModal extends Modal {
-	private root: Root;
+    private root: Root;
 
-	constructor(app: App) {
-		super(app);
-	}
+    constructor(app: App) {
+        super(app);
+    }
 
-	onOpen() {
-		const { contentEl, app } = this;
-		this.root = createRoot(contentEl);
-		const AppContext = React.createContext<App>(app);
+    onOpen() {
+        const { contentEl, app } = this;
+        this.root = createRoot(contentEl);
 
-		this.root.render(
-			<AppContext.Provider value={app}>
-				<h4>Hello, Alberto!</h4>
-			</AppContext.Provider>
-		);
-	}
+        this.root.render(
+            <AppContext.Provider value={app}>
+                <h4>Hello, Alberto!</h4>
+            </AppContext.Provider>
+        );
+    }
 }
 
 class AppSettingTab extends PluginSettingTab {
-	plugin: AppPlugin;
+    plugin: AppPlugin;
 
-	constructor(app: App, plugin: AppPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+    constructor(app: App, plugin: AppPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	display(): void {
-		const { containerEl } = this;
+    display(): void {
+        const { containerEl } = this;
 
-		containerEl.empty();
+        containerEl.empty();
 
-		new Setting(containerEl)
-			.setName("Setting #1")
-			.setDesc("It's a secret")
-			.addText((text) =>
-				text
-					.setPlaceholder("Enter your secret")
-					.setValue(this.plugin.settings.mySetting)
-					.onChange(async (value) => {
-						this.plugin.settings.mySetting = value;
-						await this.plugin.saveSettings();
-					})
-			);
-	}
+        new Setting(containerEl)
+            .setName("Setting #1")
+            .setDesc("It's a secret")
+            .addText((text) =>
+                text
+                    .setPlaceholder("Enter your secret")
+                    .setValue(this.plugin.settings.mySetting)
+                    .onChange(async (value) => {
+                        this.plugin.settings.mySetting = value;
+                        await this.plugin.saveSettings();
+                    })
+            );
+    }
 }
